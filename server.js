461344f2f939961042e44a92b95a5e321716645f
olenaageyeva <olenaageyeva@gmail.com>
2023-08-05 10:09:44 -0400

    added query params

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.get('/api/express_backend', async (req, res) => {
-    const user = req.query.user || "";
-    const repo = req.query.repo || "";
-    const open = req.query.open || "open";
+    const { user = "", repo = "", open = "open", per_page = 30, page = 1 } = req.query
     const baseUrl = "https://api.github.com/search/issues";
-    const fullUrl = `${baseUrl}?q=repo:${user}/${repo}+type:issue+state:${open}`;
+    const  q= `repo:${user}/${repo}+type:issue+state:${open}`;
+
+    const fullUrl = `${baseUrl}?q=${q}&per_page=${per_page}&page=${page}`;
     const headers = {
         Authorization: process.env.GIT_TOKEN,
         Accept: "application/json",
@@ -27,6 +27,6 @@ app.get('/api/express_backend', async (req, res) => {
         const response = await axios.get(fullUrl, { headers })
         res.send(response.data);
     } catch (err) {
-        console.log("Error",err)
+        console.log("Error", err)
     }
 }); 
